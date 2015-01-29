16de0996c29b754b16760b2916d0889e0c68aba8
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-01-29 15:21:31 -0430

    index panel

--- application/views/panel/index.php
+++ application/views/panel/index.php
@@ -5,83 +5,44 @@
 </section>
 
 <!-- Main content -->
+
+
 <section class="content">
-	<div class="row">
-		<div class="col-lg-3 col-xs-6">
-			<!-- small box -->
-			<div class="small-box bg-aqua">
-				<div class="inner">
-					<h3>
-						150
-					</h3>
-					<p>
-						New Orders
-					</p>
+	<div class="panel-body">  
+        <!--/stories-->
+			<div class="row panel-row">    
+				<br>
+				<div class="col-md-2 col-sm-3 text-center">
+				  	<a class="story-img" href="#"><img src="./public/img/upload.png" style="width:100px;height:100px" class="img-circle"></a>
 				</div>
-				<div class="icon">
-					<i class="ion ion-bag"></i>
+				<div class="col-md-10 col-sm-9">
+				  	<h3>Nombre del Paciente</h3>
+				  	<div class="row">
+				    	<div class="col-xs-9">
+				      		<p>Consultorio y Dirección.</p>
+				      		<p class="lead"><span class="label label-default">2.000 Bs</span></p>
+				      		<p class="pull-right">
+				      			<span class="label label-default bg-green"><i class="fa fa-check"></i> Aprobado</span> 
+				      			<span class="label label-default bg-red"><i class="fa fa-times"></i> Rechasado</span> 
+				      			<span class="label label-default bg-yellow"><i class="fa fa-refresh"></i> Esperando</span></p>
+				      		<ul class="list-inline">
+				      			<li>
+				      				<a href="#"><i class="glyphicon glyphicon-time"></i> 03:00 pm</a>
+				      			</li>
+				      			<li>
+				      				<a href="#"><i class="glyphicon glyphicon-calendar"></i> 02/02/15</a>
+				      			</li>
+				      			<li>
+				      				<a href="#"><i class="glyphicon glyphicon-user"></i> Especialidad</a>
+				      			</li>
+				      		</ul>
+				      	</div>
+				    	<div class="col-xs-3"></div>
+					</div>
+					<br><br>
 				</div>
-				<a href="#" class="small-box-footer">
-					More info <i class="fa fa-arrow-circle-right"></i>
-				</a>
 			</div>
-		</div><!-- ./col -->
-		<div class="col-lg-3 col-xs-6">
-			<!-- small box -->
-			<div class="small-box bg-green">
-				<div class="inner">
-					<h3>
-						53<sup style="font-size: 20px">%</sup>
-					</h3>
-					<p>
-						Bounce Rate
-					</p>
-				</div>
-				<div class="icon">
-					<i class="ion ion-stats-bars"></i>
-				</div>
-				<a href="#" class="small-box-footer">
-					More info <i class="fa fa-arrow-circle-right"></i>
-				</a>
-			</div>
-		</div><!-- ./col -->
-		<div class="col-lg-3 col-xs-6">
-			<!--- small box -->
-			<div class="small-box bg-yellow">
-				<div class="inner">
-					<h3>
-						44
-					</h3>
-					<p>
-						User Registrations
-					</p>
-				</div>
-				<div class="icon">
-					<i class="ion ion-person-add"></i>
-				</div>
-				<a href="#" class="small-box-footer">
-					More info <i class="fa fa-arrow-circle-right"></i>
-				</a>
-			</div>
-		</div><!-- ./col -->
-		<div class="col-lg-3 col-xs-6">
-			<!-- small box -->
-			<div class="small-box bg-red">
-				<div class="inner">
-					<h3>
-						65
-					</h3>
-					<p>
-						Unique Visitors
-					</p>
-				</div>
-				<div class="icon">
-					<i class="ion ion-pie-graph"></i>
-				</div>
-				<a href="#" class="small-box-footer">
-					More info <i class="fa fa-arrow-circle-right"></i>
-				</a>
-			</div>
-		</div><!-- ./col -->
-	</div>
+			<hr>
+        <!--/stories-->          
+    </div>
 </section>
