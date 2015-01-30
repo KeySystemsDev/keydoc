dc6b2d091d495eb89095c28e4d83bca7cd4da61b
Diego Carciente <diego.carciente@gmail.com>
2015-01-30 17:09:26 -0430

    cambio en panel vista init

--- application/views/panel/index.php
+++ application/views/panel/index.php
@@ -9,40 +9,50 @@
 
 <section class="content">
 	<div class="panel-body">  
-        <!--/stories-->
-			<div class="row panel-row">    
-				<br>
-				<div class="col-md-2 col-sm-3 text-center">
-				  	<a class="story-img" href="#"><img src="./public/img/upload.png" style="width:100px;height:100px" class="img-circle"></a>
-				</div>
-				<div class="col-md-10 col-sm-9">
-				  	<h3>Nombre del Paciente</h3>
-				  	<div class="row">
-				    	<div class="col-xs-9">
-				      		<p>Consultorio y Dirección.</p>
-				      		<p class="lead"><span class="label label-default">2.000 Bs</span></p>
-				      		<p class="pull-right">
-				      			<span class="label label-default bg-green"><i class="fa fa-check"></i> Aprobado</span> 
-				      			<span class="label label-default bg-red"><i class="fa fa-times"></i> Rechasado</span> 
-				      			<span class="label label-default bg-yellow"><i class="fa fa-refresh"></i> Esperando</span></p>
-				      		<ul class="list-inline">
-				      			<li>
-				      				<a href="#"><i class="glyphicon glyphicon-time"></i> 03:00 pm</a>
-				      			</li>
-				      			<li>
-				      				<a href="#"><i class="glyphicon glyphicon-calendar"></i> 02/02/15</a>
-				      			</li>
-				      			<li>
-				      				<a href="#"><i class="glyphicon glyphicon-user"></i> Especialidad</a>
-				      			</li>
-				      		</ul>
-				      	</div>
-				    	<div class="col-xs-3"></div>
+        <?php 
+	        foreach ($this->notificacion_doctor as $key) {
+	        	if($key->status_aprobado_cita == 1){
+	        		$status = '<span class="label bg-green"><i class="fa fa-check"></i> Aprobado</span>';     			        		
+	        	}else if($key->status_pendiente_cita == 1){
+	        		$status = '<span class="label bg-blue"><i class="fa fa-refresh"></i> &nbsp;Pendiente por aprobación</span>';
+	        	}else if($key->status_rechazado_cita == 1){
+	        		$status = '<span class="label bg-red"><i class="fa fa-times"></i> &nbsp;Rechazado</span>';
+	        	}
+				echo 
+	        	'<div class="row panel-row">    
+					<br>
+					<div class="col-md-2 col-sm-3 text-center">
+					  	<a class="story-img" href="#"><img src="'.base_url().$key->url_imagen_perfil.'" style="width:100px;height:100px" class="img-circle"></a>
 					</div>
-					<br><br>
+					<div class="col-md-10 col-sm-9">
+					  	<h3>'.$key->nombre_paciente.'</h3>
+					  	<div class="row">
+					    	<div class="col-xs-9">
+					      		<p>Consultorio: '.$key->nombre_consultorio.'.</p>
+					      		<p>Referencia de la ubicación: '.$key->direccion_consultorio.'.</p>
+					      		<p class="lead pull-right">
+					      			<span class="label label-default">'.number_format($key->costo_horario, 2, ',', '.').'</span>
+					      			'.$status.'	
+					      		</p>
+					      		<ul class="list-inline lead ">
+					      			<li>
+					      				<i class="glyphicon glyphicon-time"></i> '.$key->desde_hora_horario.' - '.$key->hasta_hora_horario.'
+					      			</li>
+					      			<li>
+					      				<i class="glyphicon glyphicon-calendar"></i> '.$key->fecha_horario.'
+					      			</li>
+					      			<li>
+					      				<i class="glyphicon glyphicon-user"></i> '.$key->especialidad.'
+					      			</li>
+					      		</ul>
+					      	</div>
+					    	<div class="col-xs-3"></div>
+						</div>
+						<br><br>
+					</div>
 				</div>
-			</div>
-			<hr>
-        <!--/stories-->          
-    </div>
+				<hr>';
+	        }
+		?>         
+    </div>    
 </section>
