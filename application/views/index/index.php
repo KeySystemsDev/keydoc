3fedcf89ce315b011fec16c23686b3dddd51acd1
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-02-02 18:08:41 -0430

    img top

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -353,7 +353,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-8 col-lg-offset-2">
-					<div class="modal-body">
+					<div class="modal-body modal-margin">
 						<h2>Paciente</h2>
 						<img class="img-responsive" src="<?php echo base_url()?>public/libs/pagina/img/portfolio/composicion_paciente_1.jpg" alt="">
 						<!--<p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
@@ -383,7 +383,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-8 col-lg-offset-2">
-					<div class="modal-body">
+					<div class="modal-body modal-margin">
 						<h2>Médico</h2>						
 						<img class="img-responsive img-centered" src="<?php echo base_url()?>public/libs/pagina/img/portfolio/composision_doctor_1.jpg" alt="">
 						<!--<p><a href="http://designmodo.com/startup/?u=787">Startup Framework</a> is a website builder for professionals. Startup Framework contains components and complex blocks (PSD+HTML Bootstrap themes and templates) which can easily be integrated into almost any design. All of these components are made in the same style, and can easily be integrated into projects, allowing you to create hundreds of solutions for your future projects.</p>
@@ -407,7 +407,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-8 col-lg-offset-2">
-					<div class="modal-body">
+					<div class="modal-body modal-margin">
 						<h2>Recepción</h2>
 						<img class="img-responsive img-centered" src="<?php echo base_url()?>public/libs/pagina/img/portfolio/composicion_recepcion_1.jpg" alt="">
 						<!--<p>Treehouse is a free PSD web template built by <a href="https://www.behance.net/MathavanJaya">Mathavan Jaya</a>. This is bright and spacious design perfect for people or startup companies looking to showcase their apps or other projects.</p>
